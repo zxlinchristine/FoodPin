e05383d777295944baa3e0f5ce3880c2b9b923c3
LinZihRong <410721370@gms.ndhu.edu.tw>
2021-11-01 16:12:33 +0800

    first commit

---
+++ FoodPin/TableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  TableViewController.swift
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2021/11/1.
+//
+
+import UIKit
+
+
+enum Section {  //suppose one section only
+    case all
+}
+
+class TableViewController: UITableViewController {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    lazy var dataSource = configureDataSource()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = dataSource
+                
+        //Create the snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(restaurantNames, toSection: .all)
+
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    
+    
+    
+    // MARK: - UITableView Diffable Data Source
+    
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+        let cellIdentifier = "datacell"
+        
+        let dataSource = UITableViewDiffableDataSource<Section, String>(
+            tableView: tableView,
+            cellProvider: {  tableView, indexPath, restaurantName in
+                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+                
+                cell.textLabel?.text = restaurantName
+                cell.imageView?.image = UIImage(named: self.restaurantImages[indexPath.row])
+                return cell
+            }
+        )
+        
+        return dataSource
+    }
+    
+    
+}
